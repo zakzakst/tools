df61e4983f88e4815dafa9554e4f54f0f0b587a7
zakzakst <e1433104@gmai.com>
2025-02-04 22:10:03 +0900

    修正

--- src/app/sample/(two-column)/layout.tsx
+++ src/app/sample/(two-column)/layout.tsx
@@ -2,12 +2,14 @@ import "@ant-design/v5-patch-for-react-19";
 
 export default function RootLayout({
   children,
+  sidebar,
 }: Readonly<{
   children: React.ReactNode;
+  sidebar: React.ReactNode;
 }>) {
   return (
     <>
-      <aside>サイドバー</aside>
+      <aside>{sidebar}</aside>
       <main>
         <div className="h-screen overflow-y-auto">two column</div>
         <div className="p-4">{children}</div>
